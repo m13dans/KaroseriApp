c394d5952b5ad995fa605680d27c8a6ac69c68fa
Arya <danuraryapratama@gmail.com>
2024-11-06 11:48:44 +0700

    add gitignore

--- KaroseriApp.Application/obj/Debug/net8.0/KaroseriApp.Application.AssemblyInfo.cs
+++ KaroseriApp.Application/obj/Debug/net8.0/KaroseriApp.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaroseriApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b16cbeb44e137e5067f03d86a13ec8b909fffa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaroseriApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaroseriApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KaroseriApp.Web/obj/Debug/net8.0/KaroseriApp.Web.AssemblyInfo.cs
+++ KaroseriApp.Web/obj/Debug/net8.0/KaroseriApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaroseriApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b16cbeb44e137e5067f03d86a13ec8b909fffa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaroseriApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaroseriApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
